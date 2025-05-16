e4759517fb1cda43e84556bd84caeaa66bb0275c
rlatnfla <rlatnfla0724@gmail.com>
2025-05-16 13:56:34 +0900

    feat: Username/Password 인증시 사용될 커스텀 UserDetails 클래스 추가

---
+++ src/main/java/com/parksupark/soomjae/server/auth/username/dto/UsernamePasswordUserDetails.java
@@ -0,0 +1,50 @@
+package com.parksupark.soomjae.server.auth.username.dto;
+
+import com.parksupark.soomjae.server.member.entity.Member;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UsernamePasswordUserDetails implements UserDetails {
+    private final transient Member member;
+
+    public UsernamePasswordUserDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
