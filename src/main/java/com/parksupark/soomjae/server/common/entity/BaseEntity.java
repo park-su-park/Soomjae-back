841f640e505dbad2e5e3ace8c29faba4789051af
haha4825 <tw086219@gmail.com>
2025-05-23 13:37:15 +0900

    [Feat] 베이스 엔티티 추가

---
+++ src/main/java/com/parksupark/soomjae/server/common/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.parksupark.soomjae.server.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedTime;
+
+    private LocalDateTime deletedTime;
+
+    public void markDeleted() {
+        this.deletedTime = LocalDateTime.now();
+    }
+    // getter 생략 가능 (필요시 추가)
+}
+
