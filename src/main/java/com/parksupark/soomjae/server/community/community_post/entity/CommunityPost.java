544521cc750ee919914dd72ef06bd2c9caff505f
haha4825 <tw086219@gmail.com>
2025-05-27 23:39:17 +0900

    [Feat] CommunityPost Response를 위해 엔티티에 어노테이션 추가

--- src/main/java/com/parksupark/soomjae/server/community/community_post/entity/CommunityPost.java
+++ src/main/java/com/parksupark/soomjae/server/community/community_post/entity/CommunityPost.java
@@ -10,8 +10,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class CommunityPost extends BaseEntity {
 
     @Id
