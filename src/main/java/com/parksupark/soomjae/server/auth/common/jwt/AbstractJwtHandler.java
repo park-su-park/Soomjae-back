9a614dfcc38b4cdfbb835b85314217c5b05302c1
rlatnfla <rlatnfla0724@gmail.com>
2025-05-28 16:37:43 +0900

    refactor: JwtProvider 에 JwtHandler 를 주입하는 방식으로 리팩토링 -> 테스트시 알맞는 JwtHandler 를 주입하는 것으로 동작을 손쉽게 바꿀 수 있음

---
+++ src/main/java/com/parksupark/soomjae/server/auth/common/jwt/AbstractJwtHandler.java
@@ -0,0 +1,43 @@
+package com.parksupark.soomjae.server.auth.common.jwt;
+
+import com.parksupark.soomjae.server.member.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import java.util.Base64;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public abstract class AbstractJwtHandler implements JwtHandler{
+
+    protected final SecretKey key;
+
+    protected AbstractJwtHandler(String secret) {
+        byte[] keyBytes = Base64.getDecoder().decode(secret);
+        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    @Override
+    public Claims parse(String token) {
+        return Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+    protected abstract Date getIssuedAt();
+
+    protected abstract Date getExpiration();
+
+    @Override
+    public String generate(String subject, Role role) {
+        return Jwts.builder()
+            .subject(subject)
+            .claim("role", role.getKey())
+            .issuedAt(getIssuedAt())
+            .expiration(getExpiration())
+            .signWith(key)
+            .compact();
+    }
+}
--- src/main/java/com/parksupark/soomjae/server/auth/common/jwt/DefaultJwtHandler.java
+++ src/main/java/com/parksupark/soomjae/server/auth/common/jwt/DefaultJwtHandler.java
@@ -1,43 +1,23 @@
 package com.parksupark.soomjae.server.auth.common.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
 import java.util.Date;
 
 @Component
-public class DefaultJwtHandler implements JwtHandler {
-    private final SecretKey key;
+public class DefaultJwtHandler extends AbstractJwtHandler {
 
     public DefaultJwtHandler(@Value("${jwt.secret}") String secret) {
-        byte[] keyBytes = Base64.getDecoder().decode(secret);
-        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+        super(secret);
     }
 
     @Override
-    public String generate(String subject) {
-        long nowMillis = System.currentTimeMillis();
-        long expirationMillis = nowMillis + 1000 * 60 * 60;
-
-        return Jwts.builder()
-                .subject(subject)
-                .signWith(key)
-                .issuedAt(new Date(nowMillis))
-                .expiration(new Date(expirationMillis))
-                .compact();
+    protected Date getIssuedAt() {
+        return new Date();
     }
 
     @Override
-    public Claims parse(String token) {
-        return Jwts.parser()
-                .verifyWith(key)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+    protected Date getExpiration() {
+        return new Date(System.currentTimeMillis() + 3600000); // 1시간 후
     }
 }
