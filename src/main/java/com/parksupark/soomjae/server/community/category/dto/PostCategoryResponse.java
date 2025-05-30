4091fee7cc41e93ae237e0f038aab09ee712eeb3
haha4825 <tw086219@gmail.com>
2025-05-30 17:27:52 +0900

    [Feat] 커뮤니티 포스트 응답에 들어갈 Post Category 응답 dto(계층이나 child없이 이름만 들어감)

---
+++ src/main/java/com/parksupark/soomjae/server/community/category/dto/PostCategoryResponse.java
@@ -0,0 +1,18 @@
+package com.parksupark.soomjae.server.community.category.dto;
+
+import com.parksupark.soomjae.server.community.category.entity.Category;
+import lombok.Getter;
+
+@Getter
+public class PostCategoryResponse {
+
+    String name;
+
+    public PostCategoryResponse(String name) {
+        this.name = name;
+    }
+
+    public static PostCategoryResponse of(Category category) {
+        return new PostCategoryResponse(category.getName());
+    }
+}
