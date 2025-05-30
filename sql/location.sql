f32cd0e57b7d87906caa5166c586e85410fea7b1
haha4825 <tw086219@gmail.com>
2025-05-31 01:08:25 +0900

    [fix] location 관련 csv, sql 파일 수정

--- sql/location.sql
+++ sql/location.sql
@@ -1,17 +1,16 @@
-CREATE TABLE location
-(
-    code       BIGINT PRIMARY KEY,
-    sido       TEXT,
-    sigungu    TEXT,
-    eupmyeon   TEXT,
-    dongri     TEXT,
-    created_at DATE,
-    deleted_at DATE,
-    hierarchy  INT NOT NULL
+CREATE TABLE location (
+                          code BIGINT PRIMARY KEY,
+                          name TEXT NOT NULL,
+                          created_at DATE,
+                          deleted_at DATE,
+                          hierarchy INT NOT NULL,
+                          parent_code BIGINT,
+                          FOREIGN KEY (parent_code) REFERENCES location(code)
 );
 
-COPY location
-    FROM {location.csv 파일 절대 경로}
+
+COPY location(code, name, created_at, deleted_at, hierarchy, parent_code)
+    FROM 'location.csv 경로'
     DELIMITER ','
     CSV HEADER
     ENCODING 'UTF8';
