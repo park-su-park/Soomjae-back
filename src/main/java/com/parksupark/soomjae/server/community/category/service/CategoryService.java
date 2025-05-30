fc041c115a671305c77d313ddf8d8e1aa3eb1100
haha4825 <tw086219@gmail.com>
2025-05-30 17:25:12 +0900

    [Feat] 카테고리 서비스, 레포지토리 개발

---
+++ src/main/java/com/parksupark/soomjae/server/community/category/service/CategoryService.java
@@ -0,0 +1,61 @@
+package com.parksupark.soomjae.server.community.category.service;
+
+import static com.parksupark.soomjae.server.common.exception.ErrorMessages.COMMUNITY_POST_NOT_FOUND;
+import static com.parksupark.soomjae.server.community.category.constant.CategoryConstant.CATEGORY_NAME_DUPLICATE_ERROR;
+import static com.parksupark.soomjae.server.community.category.constant.CategoryConstant.PARENT_CATEGORY_NOT_FOUND;
+import static com.parksupark.soomjae.server.community.category.constant.CategoryConstant.ROOT_CATEGORY;
+import static com.parksupark.soomjae.server.community.category.constant.CategoryConstant.ROOT_CATEGORY_NOT_FOUND;
+
+import com.parksupark.soomjae.server.community.category.dto.CategoryRequestDto;
+import com.parksupark.soomjae.server.community.category.dto.CategoryResponseDto;
+import com.parksupark.soomjae.server.community.category.entity.Category;
+import com.parksupark.soomjae.server.community.category.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public Long createCategory(CategoryRequestDto categoryRequestDto) {
+        if (categoryRepository.findByName(categoryRequestDto.getName()).isPresent()) {
+            throw new IllegalStateException(CATEGORY_NAME_DUPLICATE_ERROR);
+        }
+        if (categoryRequestDto.getParentCategoryName() == null) {
+            Category rootCategory = categoryRepository.findByName(ROOT_CATEGORY)
+                    .orElseThrow(() -> new IllegalStateException(ROOT_CATEGORY_NOT_FOUND));
+            Category category = Category.builder()
+                    .name(categoryRequestDto.getName())
+                    .parent(rootCategory)
+                    .hierarchy(1)
+                    .build();
+            return categoryRepository.save(category).getId();
+        } else {
+            Category parent = categoryRepository.findByName(
+                            categoryRequestDto.getParentCategoryName())
+                    .orElseThrow(() -> new IllegalStateException(PARENT_CATEGORY_NOT_FOUND));
+
+            Category category = Category.builder()
+                    .parent(parent)
+                    .name(categoryRequestDto.getName())
+                    .hierarchy(parent.getHierarchy() + 1)
+                    .build();
+            return categoryRepository.save(category).getId();
+        }
+    }
+
+    public CategoryResponseDto readCategory(String name) {
+        Category category = categoryRepository.findByName(name)
+                .orElseThrow(() -> new IllegalStateException(COMMUNITY_POST_NOT_FOUND));
+        return CategoryResponseDto.of(category);
+    }
+
+    public CategoryResponseDto readRootCategory() {
+        Category category = categoryRepository.findByName(ROOT_CATEGORY)
+                .orElseThrow(() -> new IllegalStateException(ROOT_CATEGORY_NOT_FOUND));
+        return CategoryResponseDto.of(category);
+    }
+
+}
