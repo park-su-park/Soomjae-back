4d28cdbed61b460b26765c8514f868f499cff138
rlatnfla <rlatnfla0724@gmail.com>
2025-06-01 09:48:58 +0900

    feat: Like 도메인 엔티티, 레포지토리, 서비스, 컨트롤러, DTO 클래스 추가

--- src/main/java/com/parksupark/soomjae/server/community/like/controller/LikeController.java
+++ src/main/java/com/parksupark/soomjae/server/community/like/controller/LikeController.java
@@ -5,6 +5,7 @@ import com.parksupark.soomjae.server.community.like.dto.LikeStatusResponse;
 import com.parksupark.soomjae.server.community.like.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@PreAuthorize("isAuthenticated()")
+@PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping("/v1/boards")
 @RequiredArgsConstructor
