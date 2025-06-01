351b6fcd6f32c245eec2dbed316e15c7b583d9c5
rlatnfla <rlatnfla0724@gmail.com>
2025-06-01 10:01:00 +0900

    feat: Like 도메인 엔티티, 레포지토리, 서비스, 컨트롤러, DTO 클래스 추가

--- src/main/java/com/parksupark/soomjae/server/community/like/repository/LikeRepository.java
+++ src/main/java/com/parksupark/soomjae/server/community/like/repository/LikeRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
     Optional<Like> findByPostTypeAndPostIdAndMemberId(String postType, Long postId, Long memberId);
+
     Long countByPostTypeAndPostId(String postType, Long postId);
+
     boolean existsByPostTypeAndPostIdAndMemberId(String postType, Long postId, Long memberId);
 }
