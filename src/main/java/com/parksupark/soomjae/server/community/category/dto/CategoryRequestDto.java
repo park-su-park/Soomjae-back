2b012e4b1f4b1f0af0f0ad166adb161888146265
haha4825 <tw086219@gmail.com>
2025-06-02 15:14:13 +0900

    [Fix] 테스트 코드 상 직접 체를 생성하는 부분이 있어 생성자 추가

--- src/main/java/com/parksupark/soomjae/server/community/category/dto/CategoryRequestDto.java
+++ src/main/java/com/parksupark/soomjae/server/community/category/dto/CategoryRequestDto.java
@@ -1,8 +1,12 @@
 package com.parksupark.soomjae.server.community.category.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryRequestDto {
 
     private String name;
