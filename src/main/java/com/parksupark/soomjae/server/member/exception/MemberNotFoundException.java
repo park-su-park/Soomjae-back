486b314446e1d39ba8aac64a8bfee2f27efdf394
rlatnfla <rlatnfla0724@gmail.com>
2025-06-20 18:10:21 +0900

    feat: Member 조회 실패 예외 클래스 추가

---
+++ src/main/java/com/parksupark/soomjae/server/member/exception/MemberNotFoundException.java
@@ -0,0 +1,9 @@
+package com.parksupark.soomjae.server.member.exception;
+
+public class MemberNotFoundException extends RuntimeException {
+
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+
+}
