f67697391ce0de96ad716e2b5d2724553efa0a2a
rlatnfla <rlatnfla0724@gmail.com>
2025-06-20 18:11:24 +0900

    chore: Member 엔티티에 nickname 필드가 추가에 따른 코드 수정

--- src/main/java/com/parksupark/soomjae/server/member/dto/MemberResponse.java
+++ src/main/java/com/parksupark/soomjae/server/member/dto/MemberResponse.java
@@ -1,21 +1,10 @@
 package com.parksupark.soomjae.server.member.dto;
 
-import com.parksupark.soomjae.server.member.entity.Member;
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
+@Data
 public class MemberResponse {
-    private Long memberId;
-
-    private String email;
-
-    public MemberResponse(Long memberId, String email) {
-        this.memberId = memberId;
-        this.email = email;
-    }
-
-    public static MemberResponse of(Member member) {
-        return new MemberResponse(member.getId(), member.getEmail());
-    }
-
+    private final Long memberId;
+    private final String email;
+    private final String nickname;
 }
