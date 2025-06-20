43cc1de868fb20eb5de5f5fa5ec51588b4d0637d
rlatnfla <rlatnfla0724@gmail.com>
2025-06-20 18:10:41 +0900

    feat: crud 메서드 추가

--- src/main/java/com/parksupark/soomjae/server/member/repository/MemberRepository.java
+++ src/main/java/com/parksupark/soomjae/server/member/repository/MemberRepository.java
@@ -5,10 +5,18 @@ import java.util.Optional;
 
 public interface MemberRepository {
 
+    Optional<Member> findById(Long id);
+
     Optional<Member> findByEmail(String email);
 
     Member save(Member member);
 
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
     void delete(Member member);
+
+    void flush();
 
 }
