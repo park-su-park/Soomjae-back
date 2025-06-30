9b78a60a2d31818e1567eb1972b83c90cd9a7b93
rlatnfla <rlatnfla0724@gmail.com>
2025-06-30 18:06:38 +0900

    test: 테스트용 no-op MemberService 구현체 추가

---
+++ src/test/java/com/parksupark/soomjae/server/member/service/NoOpMemberService.java
@@ -0,0 +1,32 @@
+package com.parksupark.soomjae.server.member.service;
+
+import com.parksupark.soomjae.server.member.dto.CreateMemberRequest;
+import com.parksupark.soomjae.server.member.dto.MemberResponse;
+
+public class NoOpMemberService implements MemberService {
+
+    @Override
+    public MemberResponse updateNickname(Long id, String nickname) {
+        return null;
+    }
+
+    @Override
+    public MemberResponse updatePassword(Long id, String password) {
+        return null;
+    }
+
+    @Override
+    public MemberResponse updateEmail(Long id, String email) {
+        return null;
+    }
+
+    @Override
+    public MemberResponse readMember(Long id) {
+        return null;
+    }
+
+    @Override
+    public MemberResponse createMember(CreateMemberRequest request) {
+        return null;
+    }
+}
