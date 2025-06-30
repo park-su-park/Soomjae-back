808e6c58bc09a40016ffd23ebe0755dc100d7b65
rlatnfla <rlatnfla0724@gmail.com>
2025-07-01 01:05:46 +0900

    chore: Getter 만 사용하게 수정

--- src/main/java/com/parksupark/soomjae/server/member/dto/MemberResponse.java
+++ src/main/java/com/parksupark/soomjae/server/member/dto/MemberResponse.java
@@ -1,10 +1,16 @@
 package com.parksupark.soomjae.server.member.dto;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class MemberResponse {
     private final Long memberId;
     private final String email;
     private final String nickname;
+
+    public MemberResponse(Long memberId, String email, String nickname) {
+        this.memberId = memberId;
+        this.email = email;
+        this.nickname = nickname;
+    }
 }
