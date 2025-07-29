ee5dfa50964a41b1f0d197802014d7246fbc5793
haha4825 <tw086219@gmail.com>
2025-07-29 17:57:36 +0900

    [Fix] commentResponse에 생성시간 추가

--- src/main/java/com/parksupark/soomjae/server/community/comment/dto/CommentResponse.java
+++ src/main/java/com/parksupark/soomjae/server/community/comment/dto/CommentResponse.java
@@ -2,6 +2,7 @@ package com.parksupark.soomjae.server.community.comment.dto;
 
 import com.parksupark.soomjae.server.community.comment.entity.Comment;
 import com.parksupark.soomjae.server.member.dto.MemberResponse;
+import java.time.Instant;
 import lombok.Getter;
 
 @Getter
@@ -13,14 +14,18 @@ public class CommentResponse {
 
     private MemberResponse author;
 
-    private CommentResponse(Long commentId, String content, MemberResponse author) {
+    private Instant createdTime;
+
+
+    private CommentResponse(Long commentId, String content, MemberResponse author, Instant createdTime) {
         this.commentId = commentId;
         this.content = content;
         this.author = author;
+        this.createdTime = createdTime;
     }
 
     public static CommentResponse of(Comment comment) {
         return new CommentResponse(comment.getId(), comment.getContent(),
-                MemberResponse.of(comment.getMember()));
+                MemberResponse.of(comment.getMember()), comment.getCreatedTime());
     }
 }
