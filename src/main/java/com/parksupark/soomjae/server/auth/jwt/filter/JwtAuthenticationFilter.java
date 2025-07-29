5def81e5ddf49c13096a4d50fd529c86b1b1330d
rlatnfla <rlatnfla0724@gmail.com>
2025-07-30 00:08:52 +0900

    refactor: access, refresh 분리

--- src/main/java/com/parksupark/soomjae/server/auth/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/parksupark/soomjae/server/auth/jwt/filter/JwtAuthenticationFilter.java
@@ -22,7 +22,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         FilterChain filterChain) throws ServletException, IOException {
 
         String token = jwtProvider.resolveToken(request);
-        if (token != null && jwtProvider.validateToken(token)) {
+        if (token != null && jwtProvider.validateAccessToken(token)) {
             Authentication authentication = jwtProvider.getAuthentication(token);
 
             // thread safety
