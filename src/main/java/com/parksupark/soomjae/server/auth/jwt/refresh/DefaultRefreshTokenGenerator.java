eec67ecabca8f88426eb5505e48ec8dff0b39080
rlatnfla <rlatnfla0724@gmail.com>
2025-07-31 23:41:12 +0900

    chore: 상수 변수 도입

--- src/main/java/com/parksupark/soomjae/server/auth/jwt/refresh/DefaultRefreshTokenGenerator.java
+++ src/main/java/com/parksupark/soomjae/server/auth/jwt/refresh/DefaultRefreshTokenGenerator.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 public class DefaultRefreshTokenGenerator extends AbstractJwtKeyHolder implements
         RefreshTokenGenerator {
 
+    private static final int WEEK_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 7;
+
     public DefaultRefreshTokenGenerator(@Value("${jwt.refresh.secret}") String secret) {
         super(secret);
     }
@@ -18,7 +20,7 @@ public class DefaultRefreshTokenGenerator extends AbstractJwtKeyHolder implement
     public String generate(String subject) {
         long nowMillis = System.currentTimeMillis();
         Date now = new Date(nowMillis);
-        Date expiry = new Date(nowMillis + 1000 * 60 * 60 * 24 * 7);
+        Date expiry = new Date(nowMillis + WEEK_IN_MILLISECONDS);
 
         return Jwts.builder()
                 .subject(subject)
