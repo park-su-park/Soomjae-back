43f25dc2b6ffd874c14ef1985035d96356d0daad
haha4825 <tw086219@gmail.com>
2025-08-02 14:02:45 +0900

    [refactor] 리팩토링

--- src/main/java/com/parksupark/soomjae/server/community/communitypost/dto/CommunityPostResponse.java
+++ src/main/java/com/parksupark/soomjae/server/community/communitypost/dto/CommunityPostResponse.java
@@ -1,10 +1,7 @@
 package com.parksupark.soomjae.server.community.communitypost.dto;
 
-import com.parksupark.soomjae.server.community.category.entity.Category;
 import com.parksupark.soomjae.server.community.communitypost.entity.CommunityPost;
-import com.parksupark.soomjae.server.community.location.entity.Location;
 import com.parksupark.soomjae.server.member.dto.MemberResponse;
-import com.parksupark.soomjae.server.member.entity.Member;
 import java.time.Instant;
 import lombok.Getter;
 
@@ -31,36 +28,21 @@ public class CommunityPostResponse {
     private final Long commentNum;
 
 
-    public CommunityPostResponse(Long postId, String title, String content, MemberResponse author,
-        String category, String location, Instant createdTime, Long commentNum) {
-        this.postId = postId;
+    private CommunityPostResponse(CommunityPost communityPost, Long commentNum) {
+        this.postId = communityPost.getId();
         this.postType = "community";
-        this.title = title;
-        this.content = content;
-        this.author = author;
-        this.category = category;
-        this.location = location;
-        this.createdTime = createdTime;
+        this.title = communityPost.getTitle();
+        this.content = communityPost.getContent();
+        this.author = MemberResponse.of(communityPost.getMember());
+        this.createdTime = communityPost.getCreatedTime();
+        this.category =
+            communityPost.getCategory() != null ? communityPost.getCategory().getName() : null;
+        this.location =
+            communityPost.getLocation() != null ? communityPost.getLocation().getName() : null;
         this.commentNum = commentNum;
     }
 
     public static CommunityPostResponse of(CommunityPost communityPost, Long commentNum) {
-        Member author = communityPost.getMember();
-
-        Category category = communityPost.getCategory();
-        String categoryName = null;
-        if (category != null) {
-            categoryName = category.getName();
-        }
-
-        Location location = communityPost.getLocation();
-        String locationName = null;
-        if (location != null) {
-            locationName = location.getName();
-        }
-
-        return new CommunityPostResponse(communityPost.getId(), communityPost.getTitle(),
-            communityPost.getContent(), MemberResponse.of(author), categoryName, locationName,
-            communityPost.getCreatedTime(), commentNum);
+        return new CommunityPostResponse(communityPost, commentNum);
     }
 }
