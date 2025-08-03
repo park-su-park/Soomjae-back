0b7492de7c1016ffe09cd51bcac15e6fb7c04c47
rlatnfla <rlatnfla0724@gmail.com>
2025-08-03 20:35:34 +0900

    fix: resolve conflict

--- src/main/java/com/parksupark/soomjae/server/member/dto/MemberResponse.java
+++ src/main/java/com/parksupark/soomjae/server/member/dto/MemberResponse.java
@@ -1,5 +1,6 @@
 package com.parksupark.soomjae.server.member.dto;
 
+import com.parksupark.soomjae.server.member.Role;
 import com.parksupark.soomjae.server.member.entity.Member;
 import java.time.Instant;
 import lombok.Getter;
@@ -12,20 +13,22 @@ public class MemberResponse {
     private final String nickname;
     private final Instant createdTime;
     private final Instant modifiedTime;
+    private final Role role;
 
-    public MemberResponse(Long memberId, String email, String nickname,
+    public MemberResponse(Long memberId, String email, String nickname, Role role,
                           Instant createdTime, Instant modifiedTime) {
 
         this.memberId = memberId;
         this.email = email;
         this.nickname = nickname;
+        this.role = role;
         this.createdTime = createdTime;
         this.modifiedTime = modifiedTime;
     }
 
     public static MemberResponse of(Member member) {
         return new MemberResponse(member.getId(), member.getEmail(), member.getNickname(),
-            member.getCreatedTime(), member.getModifiedTime());
+            member.getRole(), member.getCreatedTime(), member.getModifiedTime());
     }
 
 }
