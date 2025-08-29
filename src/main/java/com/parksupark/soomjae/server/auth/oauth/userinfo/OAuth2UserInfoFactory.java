a95f19379767c5d9dacf621f11c16a14345bbf6e
rlatnfla <rlatnfla0724@gmail.com>
2025-08-29 21:58:52 +0900

    chore: 에러메시지 추가

--- src/main/java/com/parksupark/soomjae/server/auth/oauth/userinfo/OAuth2UserInfoFactory.java
+++ src/main/java/com/parksupark/soomjae/server/auth/oauth/userinfo/OAuth2UserInfoFactory.java
@@ -2,6 +2,7 @@ package com.parksupark.soomjae.server.auth.oauth.userinfo;
 
 import com.parksupark.soomjae.server.auth.oauth.AuthProvider;
 import com.parksupark.soomjae.server.auth.oauth.exception.OAuth2AuthenticationProcessingException;
+import com.parksupark.soomjae.server.common.exception.ErrorMessages;
 import java.util.Map;
 
 public class OAuth2UserInfoFactory {
@@ -11,8 +12,8 @@ public class OAuth2UserInfoFactory {
 
         return switch (provider) {
             case GOOGLE -> new GoogleOAuth2UserInfo(attributes);
-            default ->
-                throw new OAuth2AuthenticationProcessingException(provider + " 로그인은 제공하지 않습니다.");
+            default -> throw new OAuth2AuthenticationProcessingException(
+                ErrorMessages.OAUTH2_PROVIDER_NOT_SUPPORT_MESSAGE + provider);
         };
     }
 
