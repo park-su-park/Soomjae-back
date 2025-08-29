37205c413f83f559da7d9cbd7f1239ffc548cd5e
rlatnfla <rlatnfla0724@gmail.com>
2025-08-29 21:57:55 +0900

    feat: 이메일 검증 여부 체크 로직 추가

--- src/main/java/com/parksupark/soomjae/server/auth/oauth/userinfo/GoogleOAuth2UserInfo.java
+++ src/main/java/com/parksupark/soomjae/server/auth/oauth/userinfo/GoogleOAuth2UserInfo.java
@@ -3,6 +3,21 @@ package com.parksupark.soomjae.server.auth.oauth.userinfo;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 
+
+/**
+ * Google OAuth2 UserInfo 구현체
+ * <p>Google OAuth2 API의 응답 구조에 마춰 사용자 정보를 추출</p>
+ *
+ * <pre>
+ * Google 응답 예시:
+ * {
+ *      "sub": "1234567890,
+ *      "email": "example@gmail.com",
+ *      "email_verified": true,
+ *      "name": "홍길동"
+ * }
+ * </pre>
+ */
 @RequiredArgsConstructor
 public class GoogleOAuth2UserInfo implements OAuth2UserInfo {
 
@@ -17,5 +32,10 @@ public class GoogleOAuth2UserInfo implements OAuth2UserInfo {
     @Override
     public String getProviderId() {
         return (String) attributes.get("sub");
+    }
+
+    @Override
+    public boolean isEmailVerified() {
+        return Boolean.TRUE.equals(attributes.get("email_verified"));
     }
 }
