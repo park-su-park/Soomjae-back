da7dc088b7d04a83b9307e5cb894e4663a56d14b
rlatnfla <rlatnfla0724@gmail.com>
2025-08-29 21:57:45 +0900

    feat: 이메일 검증 여부 체크 로직 추가

--- src/main/java/com/parksupark/soomjae/server/auth/oauth/service/CustomOAuth2UserService.java
+++ src/main/java/com/parksupark/soomjae/server/auth/oauth/service/CustomOAuth2UserService.java
@@ -2,8 +2,10 @@ package com.parksupark.soomjae.server.auth.oauth.service;
 
 import com.parksupark.soomjae.server.auth.oauth.AuthProvider;
 import com.parksupark.soomjae.server.auth.oauth.dto.CustomOAuth2User;
+import com.parksupark.soomjae.server.auth.oauth.exception.OAuth2AuthenticationProcessingException;
 import com.parksupark.soomjae.server.auth.oauth.userinfo.OAuth2UserInfo;
 import com.parksupark.soomjae.server.auth.oauth.userinfo.OAuth2UserInfoFactory;
+import com.parksupark.soomjae.server.common.exception.ErrorMessages;
 import com.parksupark.soomjae.server.member.entity.Member;
 import com.parksupark.soomjae.server.member.repository.MemberRepository;
 import java.util.Optional;
@@ -35,7 +37,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(provider,
             oauth2User.getAttributes());
         
-        // TODO: 이메일 인증 여부 체크
+        checkEmailVerification(userInfo);
 
         Member member = findOrCreateMember(userInfo, provider);
 
@@ -43,6 +45,36 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     }
 
+    /**
+     * OAuth2 사용자의 이메일 인증 상태를 검증
+     * <p>보안상의 이유로 이메일이 인증되지 않은 사용자의 가입을 차단</p>
+     * @param userInfo: OAuth2 Provider별 OAuth2UserInfo 구현체 (GoogleOAuth2UserInfo, KakaoOAuth2UserInfo)
+     * @throws OAuth2AuthenticationProcessingException 이메일이 인증되지 않은 경우 발생
+     */
+    private void checkEmailVerification(OAuth2UserInfo userInfo) {
+        if (!userInfo.isEmailVerified()) {
+            throw new OAuth2AuthenticationProcessingException(
+                ErrorMessages.OAUTH2_EMAIL_NOT_VERIFIED_MESSAGE);
+        }
+    }
+
+
+    /**
+     * OAuth2 제공자 정보를 기반으로 기존 회원을 조회하거나 신규 회원을 생성합니다.
+     * <p>OAuth2 로그인 시 다음과 같은 로직으로 회원을 처리합니다:</p>
+     * <ul>
+     *   <li><b>기존 회원 존재:</b> 이메일 정보 업데이트 후 반환</li>
+     *   <li><b>신규 회원:</b> OAuth2 정보로 새 회원 생성 후 저장</li>
+     * </ul>
+     *
+     * <p><b>이메일 업데이트 로직:</b><br>
+     * OAuth2 제공자에서 사용자가 이메일을 변경했을 가능성이 있으므로,
+     * 기존 회원의 이메일과 OAuth2에서 받은 이메일이 다르면 자동으로 업데이트합니다.</p>
+     *
+     * @param userInfo OAuth2 제공자별 사용자 정보 (GoogleOAuth2UserInfo, KakaoOAuth2UserInfo 등)
+     * @param provider OAuth2 제공자 (GOOGLE, KAKAO, NAVER 등)
+     * @return 조회되거나 새로 생성된 Member 엔티티
+     */
     private Member findOrCreateMember(OAuth2UserInfo userInfo, AuthProvider provider) {
         Optional<Member> existingMember = memberRepository.findByProviderAndProviderId(
             provider, userInfo.getProviderId());
@@ -63,12 +95,14 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return member;
         }
 
-        Member newMember = Member.createOAuthMember(userInfo.getEmail(), provider,
+        Member newMember = Member.createOAuthMember(
+            userInfo.getEmail(),
+            provider,
             userInfo.getProviderId());
 
-        Member savedMember = memberRepository.save(newMember);
+        Member member = memberRepository.save(newMember);
         log.info("신규 OAuth2 사용자 생성: {}, provider: {}", userInfo.getEmail(), provider);
 
-        return savedMember;
+        return member;
     }
 }
