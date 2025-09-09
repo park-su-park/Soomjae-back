1da251ee0345c5bffbc86323745b1c186fb0540b
rlatnfla <rlatnfla0724@gmail.com>
2025-09-09 15:08:15 +0900

    chore: add a comment

--- src/main/java/com/parksupark/soomjae/server/member/dto/MemberBasicInfo.java
+++ src/main/java/com/parksupark/soomjae/server/member/dto/MemberBasicInfo.java
@@ -3,6 +3,10 @@ package com.parksupark.soomjae.server.member.dto;
 import com.parksupark.soomjae.server.member.Role;
 import java.time.Instant;
 
+/**
+ * <p>단순 Member 데이터 응답에 사용될 조회 성능 향상용 Projection 클래스</p>
+ * <p>(Member는 칼럼이 적어 성능 향상이 미미하긴함)</p>
+ */
 public interface MemberBasicInfo {
 
     Long getId();
