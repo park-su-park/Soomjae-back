4002f36b620e873bc02c6e897bf92d88a666b997
haha4825 <tw086219@gmail.com>
2025-09-12 16:05:32 +0900

    [fix] MeetingPost service 메소드 이름 수정

--- src/main/java/com/parksupark/soomjae/server/community/post/meetingpost/service/MeetingPostService.java
+++ src/main/java/com/parksupark/soomjae/server/community/post/meetingpost/service/MeetingPostService.java
@@ -53,7 +53,7 @@ public class MeetingPostService {
         return meetingPostRepository.save(entity).getId();
     }
 
-    public PostListResponse readByFilter(Pageable pageable,
+    public PostListResponse readMeetingPostList(Pageable pageable,
         UsernamePasswordUserDetails userDetails) {
         List<MeetingPost> posts = meetingPostRepository.findAll(pageable).getContent();
         List<PostResponse> response = getMeetingPostResponses(posts,
