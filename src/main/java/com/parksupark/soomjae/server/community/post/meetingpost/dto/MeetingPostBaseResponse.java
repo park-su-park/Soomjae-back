e7851f8d7ba985c923d8622731b49025e73bafa9
haha4825 <tw086219@gmail.com>
2025-09-16 01:57:24 +0900

    [fix] nullable추가

--- src/main/java/com/parksupark/soomjae/server/community/post/meetingpost/dto/MeetingPostBaseResponse.java
+++ src/main/java/com/parksupark/soomjae/server/community/post/meetingpost/dto/MeetingPostBaseResponse.java
@@ -2,6 +2,7 @@ package com.parksupark.soomjae.server.community.post.meetingpost.dto;
 
 import com.parksupark.soomjae.server.community.post.meetingpost.entity.MeetingPost;
 import com.parksupark.soomjae.server.member.dto.MemberResponse;
+import jakarta.annotation.Nullable;
 import java.time.Instant;
 import lombok.Getter;
 
@@ -9,14 +10,25 @@ import lombok.Getter;
 public abstract class MeetingPostBaseResponse {
 
     private final Long postId;
+
     private final String postType;
+
     private final String title;
+
     private final String content;
+
     private final MemberResponse author;
+
+    @Nullable
     private final String category;
+
+    @Nullable
     private final String location;
+
     private final Instant createdTime;
+
     private final Long likeNum;
+
     private final Boolean isLikedByMe;
 
     private final int maximumParticipants;
