715b73c66b58ca3046b42a01b111e83580a3f4dc
haha4825 <tw086219@gmail.com>
2025-09-16 01:41:44 +0900

    [fix] 생성자 접근제어 private으로 수정

--- src/main/java/com/parksupark/soomjae/server/community/participation/dto/ParticipantListResponse.java
+++ src/main/java/com/parksupark/soomjae/server/community/participation/dto/ParticipantListResponse.java
@@ -2,11 +2,12 @@ package com.parksupark.soomjae.server.community.participation.dto;
 
 import com.parksupark.soomjae.server.member.dto.MemberResponse;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ParticipantListResponse {
 
     List<MemberResponse> participants;
