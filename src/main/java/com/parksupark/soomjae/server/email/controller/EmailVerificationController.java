ff7d871f4fc6f33741284e28f6f27ef93e2fad84
rlatnfla <rlatnfla0724@gmail.com>
2025-10-07 16:27:44 +0900

    chore: checkstyle

--- src/main/java/com/parksupark/soomjae/server/email/controller/EmailVerificationController.java
+++ src/main/java/com/parksupark/soomjae/server/email/controller/EmailVerificationController.java
@@ -16,7 +16,6 @@ public class EmailVerificationController {
 
     private final EmailVerificationService emailVerificationService;
 
-
     /**
      * 이메일 인증코드 발송
      *
@@ -37,18 +36,16 @@ public class EmailVerificationController {
      * @param request 이메일 주소가 포함된 요청 객체
      * @return 발송 완료 응답 (HTTP 200)
      * @throws jakarta.validation.ConstraintViolationException
-     *  유효하지 않은 이메일 형식일 경우
+     * 유효하지 않은 이메일 형식일 경우
      * @throws com.parksupark.soomjae.server.email.exception.EmailVerificationFailedException
      * 이메일 발송 실패 시
-     * @see com.parksupark.soomjae.server.email.service.DefaultEmailVerificationService
-     * #sendVerificationCode(String)
+     * @see com.parksupark.soomjae.server.email.service.DefaultEmailVerificationService#sendVerificationCode(String)
      */
     @PostMapping("/verification")
     public DeferredResult<ResponseEntity<?>> sendVerificationCode(
         @RequestBody @Valid SendCodeRequest request) {
         return emailVerificationService.sendVerificationCode(request.getEmail());
     }
-
 
     /**
      * 이메일 인증코드 검증
@@ -70,11 +67,11 @@ public class EmailVerificationController {
      *
      * @param request 이메일 주소와 인증코드가 포함된 요청 객체
      * @return 인증 성공 메시지 (HTTP 200)
-     * @throws jakarta.validation.ConstraintViolationException 요청 파라미터 유효성 검증 실패 시
+     * @throws jakarta.validation.ConstraintViolationException
+     * 요청 파라미터 유효성 검증 실패 시
      * @throws com.parksupark.soomjae.server.common.exception.ResourceNotFoundException
      * 이메일과 코드를 사용한 EmailVerification 조회결과가 없을 경우
-     * @see com.parksupark.soomjae.server.email.service.DefaultEmailVerificationService
-     * #verifyCode(String, String)
+     * @see com.parksupark.soomjae.server.email.service.DefaultEmailVerificationService#verifyCode(String, String)
      */
     @PutMapping("/verification")
     public ResponseEntity<String> verifyCode(@RequestBody @Valid VerifyCodeRequest request) {
