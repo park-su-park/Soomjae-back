34c242c30a8dc10ec5f8cd90555aa1713e6ec8af
rlatnfla <rlatnfla0724@gmail.com>
2025-10-07 22:33:24 +0900

    chore: checkstyle

--- src/test/java/com/parksupark/soomjae/server/email/repository/NoOpEmailVerificationRepository.java
+++ src/test/java/com/parksupark/soomjae/server/email/repository/NoOpEmailVerificationRepository.java
@@ -13,6 +13,10 @@ import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuer
 
 public class NoOpEmailVerificationRepository implements EmailVerificationRepository {
 
+    public NoOpEmailVerificationRepository() {
+        super();
+    }
+
     @Override
     public void deleteByEmail(String email) {
     }
@@ -33,34 +37,127 @@ public class NoOpEmailVerificationRepository implements EmailVerificationReposit
         return false;
     }
 
-    public NoOpEmailVerificationRepository() {
-        super();
+    // ------------------------------------------------------------------------
+    // Basic CRUD Methods
+    // ------------------------------------------------------------------------
+
+    @Override
+    public <S extends EmailVerification> S save(S entity) {
+        return null;
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
+    public <S extends EmailVerification> List<S> saveAll(Iterable<S> entities) {
+        return List.of();
     }
 
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public Optional<EmailVerification> findById(Long id) {
+        return Optional.empty();
     }
 
     @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    public boolean existsById(Long id) {
+        return false;
     }
 
     @Override
-    public String toString() {
-        return super.toString();
+    public List<EmailVerification> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public List<EmailVerification> findAllById(Iterable<Long> ids) {
+        return List.of();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+    }
+
+    @Override
+    public void delete(EmailVerification entity) {
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> ids) {
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends EmailVerification> entities) {
+    }
+
+    @Override
+    public void deleteAll() {
+    }
+
+    // ------------------------------------------------------------------------
+    // Advanced JPA Methods
+    // ------------------------------------------------------------------------
+
+    @Override
+    public void flush() {
+    }
+
+    @Override
+    public <S extends EmailVerification> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends EmailVerification> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return List.of();
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<EmailVerification> entities) {
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> ids) {
+    }
+
+    @Override
+    public EmailVerification getOne(Long id) {
+        return null;
+    }
+
+    @Override
+    public EmailVerification getById(Long id) {
+        return null;
     }
 
     @Override
     public EmailVerification getReferenceById(Long id) {
         return null;
     }
+
+    // ------------------------------------------------------------------------
+    // Sorting & Paging
+    // ------------------------------------------------------------------------
+
+    @Override
+    public List<EmailVerification> findAll(Sort sort) {
+        return List.of();
+    }
+
+    @Override
+    public Page<EmailVerification> findAll(Pageable pageable) {
+        return null;
+    }
+
+    // ------------------------------------------------------------------------
+    // Example Queries
+    // ------------------------------------------------------------------------
 
     @Override
     public <S extends EmailVerification> Optional<S> findOne(Example<S> example) {
@@ -98,104 +195,27 @@ public class NoOpEmailVerificationRepository implements EmailVerificationReposit
         return null;
     }
 
+    // ------------------------------------------------------------------------
+    // Object methods
+    // ------------------------------------------------------------------------
+
     @Override
-    public void flush() {
+    public int hashCode() {
+        return super.hashCode();
     }
 
     @Override
-    public <S extends EmailVerification> S saveAndFlush(S entity) {
-        return null;
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 
     @Override
-    public <S extends EmailVerification> List<S> saveAllAndFlush(Iterable<S> entities) {
-        return List.of();
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 
     @Override
-    public void deleteAllInBatch(Iterable<EmailVerification> entities) {
-    }
-
-    @Override
-    public void deleteAllByIdInBatch(Iterable<Long> ids) {
-    }
-
-    @Override
-    public void deleteAllInBatch() {
-    }
-
-    @Override
-    public EmailVerification getOne(Long id) {
-        return null;
-    }
-
-    @Override
-    public EmailVerification getById(Long id) {
-        return null;
-    }
-
-    @Override
-    public Optional<EmailVerification> findById(Long id) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Long id) {
-        return false;
-    }
-
-    @Override
-    public void deleteById(Long id) {
-    }
-
-    @Override
-    public List<EmailVerification> findAll() {
-        return List.of();
-    }
-
-    @Override
-    public List<EmailVerification> findAllById(Iterable<Long> ids) {
-        return List.of();
-    }
-
-    @Override
-    public List<EmailVerification> findAll(Sort sort) {
-        return List.of();
-    }
-
-    @Override
-    public Page<EmailVerification> findAll(Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public <S extends EmailVerification> S save(S entity) {
-        return null;
-    }
-
-    @Override
-    public <S extends EmailVerification> List<S> saveAll(Iterable<S> entities) {
-        return List.of();
-    }
-
-    @Override
-    public void delete(EmailVerification entity) {
-    }
-
-    @Override
-    public void deleteAllById(Iterable<? extends Long> ids) {
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends EmailVerification> entities) {
-    }
-
-    @Override
-    public void deleteAll() {
-    }
-
-    @Override
-    public long count() {
-        return 0;
+    public String toString() {
+        return super.toString();
     }
 }
