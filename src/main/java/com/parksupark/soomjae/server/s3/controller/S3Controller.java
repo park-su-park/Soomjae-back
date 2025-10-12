6eb188b6ab424ab4316dde7840a704f2fbf46a50
rlatnfla <rlatnfla0724@gmail.com>
2025-10-12 21:11:54 +0900

    fix: path 수정

--- src/main/java/com/parksupark/soomjae/server/s3/controller/S3Controller.java
+++ src/main/java/com/parksupark/soomjae/server/s3/controller/S3Controller.java
@@ -6,19 +6,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/images")
 public class S3Controller {
 
     private final S3UploaderService s3UploaderService;
 
     private static final String DIR_NAME = "static";
 
-    @PostMapping("/images")
+    @PostMapping
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<String>> uploadImage(
         @RequestParam("images") List<MultipartFile> multipartFiles) {
