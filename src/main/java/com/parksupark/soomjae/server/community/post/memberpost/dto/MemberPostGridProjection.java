6f034d90b34358af0070981a5dd2da434f81bdf2
rlatnfla <rlatnfla0724@gmail.com>
2025-10-13 20:02:18 +0900

    fix: 필드명 수정

--- src/main/java/com/parksupark/soomjae/server/community/post/memberpost/dto/MemberPostGridProjection.java
+++ src/main/java/com/parksupark/soomjae/server/community/post/memberpost/dto/MemberPostGridProjection.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class MemberPostGridProjection {
 
-    private final Long postId;
+    private final Long memberPostId;
 
-    private final String imageUrl;
+    private final String image;
 }
