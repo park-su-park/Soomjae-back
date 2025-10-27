6d7c70e281a743d439ff840289c087ff63ad0593
rlatnfla <rlatnfla0724@gmail.com>
2025-10-27 23:04:20 +0900

    chore: checkstyle

--- src/main/java/com/parksupark/soomjae/server/member/util/RandomNicknameCreator.java
+++ src/main/java/com/parksupark/soomjae/server/member/util/RandomNicknameCreator.java
@@ -7,7 +7,7 @@ public class RandomNicknameCreator {
     // SecureRandom 인스턴스는 생성 비용이 비싸므로 재사용
     private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
-    public static String createRandomNickname(){
+    public static String createRandomNickname() {
         int randomSuffix = SECURE_RANDOM.nextInt(900000) + 100000;
 
         return "soomjae_user_" + randomSuffix;
