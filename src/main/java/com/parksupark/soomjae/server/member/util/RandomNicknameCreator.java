00aa99338f58cf8d0f6c69a9b046449842af245c
rlatnfla <rlatnfla0724@gmail.com>
2025-10-27 23:01:12 +0900

    fix: SecureRandom 사용

--- src/main/java/com/parksupark/soomjae/server/member/util/RandomNicknameCreator.java
+++ src/main/java/com/parksupark/soomjae/server/member/util/RandomNicknameCreator.java
@@ -1,12 +1,16 @@
 package com.parksupark.soomjae.server.member.util;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class RandomNicknameCreator {
 
-    public static String createRandomNickname() {
-        int randomSuffix = new Random().nextInt(900000) + 100000;
+    // SecureRandom 인스턴스는 생성 비용이 비싸므로 재사용
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+
+    public static String createRandomNickname(){
+        int randomSuffix = SECURE_RANDOM.nextInt(900000) + 100000;
 
         return "soomjae_user_" + randomSuffix;
     }
+
 }
