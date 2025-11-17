2bc26906d8c06431882fd33d7959beacb94b86f5
haha4825 <tw086219@gmail.com>
2025-11-18 04:48:51 +0900

    [feat] 댓글 레포지토리 코드 추가

--- src/main/java/com/parksupark/soomjae/server/community/comment/repository/CommentRepository.java
+++ src/main/java/com/parksupark/soomjae/server/community/comment/repository/CommentRepository.java
@@ -1,15 +1,20 @@
 package com.parksupark.soomjae.server.community.comment.repository;
 
 import com.parksupark.soomjae.server.community.comment.entity.Comment;
+import com.parksupark.soomjae.server.member.entity.Member;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByPostTypeAndPostIdAndDeletedTimeIsNull(String postType, Long postId);
 
     long countByPostTypeAndPostIdAndDeletedTimeIsNull(String postType, Long postId);
+
+    @Query("select c.member from Comment c where c.postType = :postType and c.postId = :postId and c.deletedTime is null")
+    List<Member> findMembersByPost(String postType, Long postId);
 
     Optional<Comment> findByIdAndDeletedTimeIsNull(Long commentId);
 }
