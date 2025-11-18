a016294f2761b179168f8e003113217fee7696f9
haha4825 <tw086219@gmail.com>
2025-11-18 19:01:11 +0900

    [fix] tokenValue 유니크 제약 조건 없앰

--- src/main/java/com/parksupark/soomjae/server/fcm/domain/Token.java
+++ src/main/java/com/parksupark/soomjae/server/fcm/domain/Token.java
@@ -33,7 +33,7 @@ public class Token {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column
     private String tokenValue;
 
     @Column(nullable = false)
