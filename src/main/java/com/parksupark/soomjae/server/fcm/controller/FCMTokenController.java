257eca3b81459fe626a5a010f5707232339e76b6
haha4825 <tw086219@gmail.com>
2025-11-19 10:55:52 +0900

    [fix] 삭제 요청 device 정보만 넘기면 되도록 변

--- src/main/java/com/parksupark/soomjae/server/fcm/controller/FCMTokenController.java
+++ src/main/java/com/parksupark/soomjae/server/fcm/controller/FCMTokenController.java
@@ -1,13 +1,13 @@
 package com.parksupark.soomjae.server.fcm.controller;
 
 import com.parksupark.soomjae.server.auth.username.dto.UsernamePasswordUserDetails;
+import com.parksupark.soomjae.server.fcm.dto.FCMTokenDelteRequest;
 import com.parksupark.soomjae.server.fcm.dto.FCMTokenRequest;
 import com.parksupark.soomjae.server.fcm.service.FCMTokenService;
 import com.parksupark.soomjae.server.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +27,8 @@ public class FCMTokenController {
         return ResponseEntity.ok("기기 토큰 저장성공");
     }
 
-    @DeleteMapping("/v1/fcm-token")
-    public ResponseEntity<String> deleteToken(@RequestBody FCMTokenRequest request,
+    @PostMapping("/v1/fcm-token/delete")
+    public ResponseEntity<String> deleteToken(@RequestBody FCMTokenDelteRequest request,
         @AuthenticationPrincipal UsernamePasswordUserDetails userDetails) {
         Member member = userDetails.getMember();
         fcmTokenService.delete(member, request);
---
+++ src/main/java/com/parksupark/soomjae/server/fcm/dto/FCMTokenDelteRequest.java
@@ -0,0 +1,12 @@
+package com.parksupark.soomjae.server.fcm.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class FCMTokenDelteRequest {
+
+    private String device;
+
+}
