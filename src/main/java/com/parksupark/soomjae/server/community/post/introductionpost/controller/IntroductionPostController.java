a7b718ecd63320525d5d7ca4c43c0aeaafe8dd15
rlatnfla <rlatnfla0724@gmail.com>
2025-11-24 13:48:19 +0900

    chore: 경로명 변경

--- src/main/java/com/parksupark/soomjae/server/community/post/introductionpost/controller/IntroductionPostController.java
+++ src/main/java/com/parksupark/soomjae/server/community/post/introductionpost/controller/IntroductionPostController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/members/introduce-post")
+@RequestMapping("/v1/members/introduction-post")
 @RequiredArgsConstructor
 public class IntroductionPostController {
 
